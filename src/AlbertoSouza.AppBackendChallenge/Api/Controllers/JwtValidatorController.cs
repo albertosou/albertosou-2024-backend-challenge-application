c926152392ef37100d13441bf96335952e64f212
Alberto Souza <albertosouza@outlook.com>
2024-08-20 05:56:04 -0300

    atualizando definições do swager

--- src/AlbertoSouza.AppBackendChallenge/Api/Controllers/JwtValidatorController.cs
+++ src/AlbertoSouza.AppBackendChallenge/Api/Controllers/JwtValidatorController.cs
@@ -1,32 +1,50 @@
 ﻿using AlbertoSouza.AppBackendChallenge.Ports;
 using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
 
-namespace AlbertoSouza.AppBackendChallenge.Api.Controllers;
-
-[Route("api/[controller]")]
-[ApiController]
-public class JwtValidatorController : ControllerBase
+namespace AlbertoSouza.AppBackendChallenge.Api.Controllers
 {
-    private readonly ILogger<JwtValidatorController> _logger;
-    private readonly IJwtValidationService _jwtValidationService;
-    public JwtValidatorController(ILogger<JwtValidatorController> logger, IJwtValidationService jwtValidationService)
+    [ApiVersion("1.0")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiController]
+    public class JwtValidatorController : ControllerBase
     {
-        _logger = logger;
-        _jwtValidationService = jwtValidationService;        
-    }
-    [HttpGet]
-    public IActionResult GetValidateJwt(string jwt)
-    {
-        try
+        private readonly ILogger<JwtValidatorController> _logger;
+        private readonly IJwtValidationService _jwtValidationService;
+
+        public JwtValidatorController(ILogger<JwtValidatorController> logger, IJwtValidationService jwtValidationService)
         {
-            var (isValid, validationMessage) = _jwtValidationService.Validate(jwt);
-            _logger.LogDebug(validationMessage);
-            return Ok(isValid);
+            _logger = logger;
+            _jwtValidationService = jwtValidationService;
         }
-        catch (Exception ex)
+
+        /// <summary>
+        /// Valida um JWT (JSON Web Token) fornecido.
+        /// </summary>
+        /// <param name="jwt">O JWT a ser validado.</param>
+        /// <returns>Um valor booleano indicando se o JWT é válido ou não.</returns>
+        [HttpGet]
+        [SwaggerOperation(
+            Summary = "Valida um JWT (JSON Web Token)",
+            Description = "Verifica se o JWT fornecido é válido.",
+            OperationId = "ValidateJwt",
+            Tags = new[] { "JWT Validator" })]
+        [SwaggerResponse(200, "Retorna um valor booleano indicando se o JWT é válido ou não.")]
+        public IActionResult GetValidateJwt(
+            [SwaggerParameter("O JWT a ser validado.", Required = true)]
+            string jwt)
         {
-            _logger.LogError($"Erro ao validar o JWT: {ex.Message}");
-            return Ok(false);
+            try
+            {
+                var (isValid, validationMessage) = _jwtValidationService.Validate(jwt);
+                _logger.LogDebug(validationMessage);
+                return Ok(isValid);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Erro ao validar o JWT: {ex.Message}");
+                return Ok(false);
+            }
         }
     }
 }
